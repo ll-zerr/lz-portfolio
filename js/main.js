7d4589901ab4adafdd5345390215e9043e8de388
laura_z <llzerr@hotmail.com>
2023-06-24 16:59:14 -0600

    Addition of js for project buttons

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 const projectButtons = document.querySelectorAll(".project-links button");
 
 projectButtons.forEach ((btn) => {
+    const textElement = btn.querySelector(".project-links a");
+    const iconElement = btn.querySelector(".project-links i");
+
+    const originalTextColor = textElement.style.color;
+    const originalIconColor = iconElement.style.color;
+
     btn.addEventListener('mouseover', (handleMouseOver) => {
         btn.style.backgroundColor = "#cac4d1";
+        textElement.style.color = "#453e4f";
+        iconElement.style.color = "#453e4f";
+    });
+
+    btn.addEventListener('mouseout', (handleMouseOut) => {
+        btn.style.backgroundColor = "#453e4f";
+        textElement.style.color = originalTextColor;
+        iconElement.style.color = originalIconColor;
     });
 });
 
-projectButtons.forEach ((btn) => {
-    btn.addEventListener('mouseout', (handleMouseOut) => {
-        btn.style.backgroundColor = "#453e4f";
-    });
-});
