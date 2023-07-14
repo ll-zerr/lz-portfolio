c564f680d8504b22d380026477899ad15ebc9d68
laura_z <llzerr@hotmail.com>
2023-07-14 14:54:10 -0600

    Addition of scroll button

--- js/main.js
+++ js/main.js
@@ -20,3 +20,19 @@ projectButtons.forEach ((btn) => {
     });
 });
 
+const scrollButton = document.getElementById("scroll-btn")
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollButton.style.display = "block";
+    } else {
+        scrollButton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
